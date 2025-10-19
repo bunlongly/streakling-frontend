7fdc8c4a8913f44077c13ea7a97177045c904db3
Bunlong <bunlongly03@gmail.com>
2025-10-19 23:54:47 +0700

    update next config js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,30 +1,24 @@
 // next.config.ts
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true 
-  },
+  outputFileTracingRoot: path.join(__dirname),
+
+  eslint: { ignoreDuringBuilds: true },
+
   images: {
     remotePatterns: [
-      // ✅ your S3 bucket
       {
         protocol: 'https',
         hostname: 'streakling.s3.ap-southeast-1.amazonaws.com',
         pathname: '/**'
       },
+      { protocol: 'https', hostname: 'img.clerk.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.clerk.dev', pathname: '/**' }
+    ],
 
-      {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-        pathname: '/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.clerk.dev',
-        pathname: '/**'
-      }
-    ]
+    formats: ['image/avif', 'image/webp']
   }
 };
 
